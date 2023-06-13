a85b0e609c40d6ef09a7d6c26b907e82c17d0134
Arsanyos <arsanyos1600@gmail.com>
2023-06-13 20:14:59 +0300

    feature/add navbar

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,22 @@
+'use client'
+
 import "./styles/globals.css";
 import { Inter } from "next/font/google";
-
+import TopBar from "./components/TopBar";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Remembered & Missed",
-  description: "Preserve and share memories of your Loved Ones",
-};
+// export const metadata = {
+//   title: "Remembered & Missed",
+//   description: "Preserve and share memories of your Loved Ones",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <>AAAAAAAAA</>
+        <nav>
+          <TopBar />
+        </nav>
         {children}
       </body>
     </html>
