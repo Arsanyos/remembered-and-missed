a0bd15f6189045e8787e65d8272e4ef7947718e8
Arsanyos <arsanyos1600@gmail.com>
2023-06-15 23:00:50 +0300

    fix responsivness in pricing page

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -13,17 +13,17 @@ const Pricing = () => {
     "Custom Notifications",
   ];
   return (
-    <div className="flex flex-col h-auto">
-      <div className="w-[100%] h-[40vh] bg-[#5C5470]"></div>
-      <div className="z-999 relative bottom-[150px] left-[15.5%] h-auto py-20 w-[70%] rounded-[25px] shadow-lg bg-[#FFFFFF] flex justify-center">
-        <div className="w-[50%] py-[95px] px-[50px] flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center h-auto">
+      <div className="w-[100vw] h-[40vh] bg-[#5C5470]"></div>
+      <div className="z-999 relative bottom-[150px] h-auto py-20 w-[80%] rounded-[25px] shadow-lg bg-[#FFFFFF] flex flex-wrap justify-center">
+        <div className="w-[100%] sm:w-[50%] py-[95px] px-[50px] flex flex-col items-center justify-center">
           <p className="text-[25px] text-[#5C5470] text-center font-Lora">
             One Price, Endless Memories For Your
           </p>
           <p className="text-[30px] text-center">Loved Ones</p>
         </div>
-        <div className="w-[50%] flex justify-center items-center">
-          <div className="w-[90%] h-auto rounded-[25px] bg-[#5C5470] py-[40px] flex flex-col items-center justify-center gap-2">
+        <div className="w-[90%] md:w-[80%] lg:w-[40%] flex justify-center items-center">
+          <div className="w-[90%] md:w-[100%] h-auto rounded-[25px] bg-[#5C5470] py-[40px] flex flex-col items-center justify-center gap-2">
             <p className="font-[Lora] font-semibold text-[24px] text-center text-[#FFFFFF]">
               Lifetime
             </p>
@@ -41,7 +41,6 @@ const Pricing = () => {
                 return (
                   <div key={index} className="flex gap-4 ">
                     <Image
-                   
                       src="/icons/Check.svg"
                       alt="green-check-icon"
                       height={15}
@@ -51,10 +50,11 @@ const Pricing = () => {
                   </div>
                 );
               })}
-              <button className=" border-solid border-4 border-[#FFFFFF] bg-[#5C5470] shadow-customFigma rounded-[25px] drop-shadow-lg py-[20px] w-[223px] h-[52px] flex justify-center items-center text-[#FFFFFF]">
+            
+            </div>
+            <button className="mt-5 mx-auto border-solid border-4 border-[#FFFFFF] bg-[#5C5470] shadow-customFigma rounded-[25px] drop-shadow-lg  w-[223px] h-[52px] flex justify-center items-center text-[#FFFFFF]">
                 GET STARTED
               </button>
-            </div>
           </div>
         </div>
       </div>
